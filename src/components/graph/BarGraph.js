04452185ece46ce029bba7c347dc3119e399bba1
ShankS3 <shashank.surwase@gmail.com>
2019-08-27 21:11:23 +0530

    Created BarGraph

---
+++ src/components/graph/BarGraph.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import { Bar } from '@vx/shape';
+import { Group } from '@vx/group';
+import { letterFrequency } from '@vx/mock-data';
+import { scaleBand, scaleLinear } from '@vx/scale';
+
+const data = letterFrequency.slice(5);
+
+const x = d => d.letter;
+const y = d => +d.frequency * 100;
+
+const BarGraph = ({ width, height }) => {
+  const xMax = width;
+  const yMax = height - 120;
+
+  const xScale = scaleBand({
+    rangeRound: [0, xMax],
+    domain: data.map(x),
+    padding: 0.4
+  });
+
+  const yScale = scaleLinear({
+    rangeRound: [yMax, 0],
+    domain: [0, Math.max(...data.map(y))]
+  });
+
+  return (
+    <svg width={width} height={height}>
+      <rect width={width} height={height} rx={14} />
+      <Group top={40}>
+        {
+          data.map((d,i) => {
+            const letter = x(d);
+            const barWidth = xScale.bandwidth();
+            const barHeight = yMax - yScale(y(d));
+            const barX = xScale(letter);
+            const barY = yMax - barHeight;
+            return(
+              <Bar
+                key={`bar-${letter}`}
+                x={barX}
+                y={barY}
+                width={barWidth}
+                height={barHeight}
+                fill="rgb(23, 233, 217)"
+              />
+            );
+          })
+        }
+      </Group>
+    </svg>
+  );
+}
+
+BarGraph.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+};
+
+export default BarGraph;
---
+++ src/components/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BarGraph from './graph/BarGraph';
+
+const Main = () => {
+  return (
+    <BarGraph width={500} height={300} />
+  );
+}
+
+export default Main;
